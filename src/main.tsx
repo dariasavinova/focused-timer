46a0206e8a1e039ad5487784216362ae5cec1fff
Daria Savinova <drsvnv77@gmail.com>
2023-06-27 20:46:21 +0400

    added redux-persist to save data to Local Storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import router from './router/router.tsx'
-import { store } from '@/store/store.ts'
+import { persistor, store } from '@/store/store.ts'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Provider store={store}>
-  <RouterProvider router={router} />
-</Provider>)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
+  </Provider>
+)
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import taskSlice from '@/store/slices/taskSlice.ts'
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const rootReducer = combineReducers({
+  taskSlice
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    taskSlice
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store)
