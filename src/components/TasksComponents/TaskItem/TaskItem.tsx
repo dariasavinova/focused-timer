bb2d002417eadfe0c361da8b35f2b4675a1ec7f6
Daria Savinova <drsvnv77@gmail.com>
2023-06-29 16:49:43 +0400

    correct behavior with click outside the dropdown

--- src/components/TasksComponents/TaskItem/TaskItem.tsx
+++ src/components/TasksComponents/TaskItem/TaskItem.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, KeyboardEvent, MouseEvent, useEffect, useRef, useState } from 'react'
+import { useClickAway } from '@uidotdev/usehooks'
 
 import styles from './TaskItem.module.scss'
 
@@ -13,30 +14,27 @@ interface TaskItemProps {
   taskName: string
 }
 
+type EventComposedPath = MouseEvent & {
+  composedPath(): Node[]
+}
+
 const TaskItem: React.FC<TaskItemProps> = ({ taskId, taskName }) => {
   const dispatch = useAppDispatch()
   const [isInputDisabled, setIsInputDisabled] = useState(true)
   const [isDropdownVisible, setIsDropdownVisible] = useState(false)
+
   const textInputRef = useRef<HTMLInputElement>(null)
-  const dropdownRef = useRef<HTMLDivElement>(null)
   const taskDetailsRef = useRef<HTMLDivElement>(null)
+  const dropdownRef = useClickAway((e: EventComposedPath) => {
+    if (e.composedPath().includes(taskDetailsRef.current!)) return
+    setIsDropdownVisible(false)
+  })
 
   useEffect(() => {
     if (!isInputDisabled && textInputRef.current) {
       textInputRef.current.focus()
     }
   }, [isInputDisabled])
-
-  useEffect(() => {
-    const handleClickOutsideDropdown = (e: any) => {
-      if (![taskDetailsRef.current, dropdownRef.current].some(element => e.composedPath().includes(element))) {
-        setIsDropdownVisible(false)
-      }
-    }
-    document.body.addEventListener('click', handleClickOutsideDropdown)
-
-    return () => document.body.removeEventListener('click', handleClickOutsideDropdown)
-  })
 
   const handleEditCurrentInput = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch(editCurrentTask({ taskId, taskName: (e.target as HTMLInputElement).value }))
@@ -63,7 +61,7 @@ const TaskItem: React.FC<TaskItemProps> = ({ taskId, taskName }) => {
         ...
       </div>
       {isDropdownVisible && (
-        <div className="task__dropdown" ref={dropdownRef}>
+        <div ref={dropdownRef}>
           <TaskItemDropdown
             setIsInputDisabled={setIsInputDisabled}
             setIsDropdownVisible={setIsDropdownVisible}
