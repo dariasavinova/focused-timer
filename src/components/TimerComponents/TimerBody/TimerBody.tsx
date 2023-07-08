8a92ed8d3fa370e7b0f598a7bc69de9a91ce95be
Daria Savinova <drsvnv77@gmail.com>
2023-07-08 21:26:45 +0400

    made total time information for task

--- src/components/TimerComponents/TimerBody/TimerBody.tsx
+++ src/components/TimerComponents/TimerBody/TimerBody.tsx
@@ -9,6 +9,12 @@ import { useAppSelector } from '@/hooks/storeDefaultHooks.ts'
 const TimerBody: React.FC = () => {
   const activeTask = useAppSelector(state => state.taskSlice.activeTask)
 
+  const resultTimeToDisplay = activeTask.totalTaskHours.hours ?
+    `Всего времени - ${activeTask.totalTaskHours.hours} ч. ${activeTask.totalTaskHours.minutes} мин.` :
+    activeTask.totalTaskHours.minutes ?
+      `Всего времени - ${activeTask.totalTaskHours.minutes} мин.` :
+      `Скоро вам будут доступны данные о вложенном в задачу времени.`
+
   return (
     <>
       <Title className={styles.description} level={5}>
@@ -16,6 +22,7 @@ const TimerBody: React.FC = () => {
         <span className={styles.description__taskName}>{activeTask.taskName}</span>
       </Title>
       <TimerCountdown />
+      <Title className={styles.time} level={5}>{resultTimeToDisplay}</Title>
     </>
   )
 }
